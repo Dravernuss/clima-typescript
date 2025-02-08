e306938fbd7af80e6f6f644765ef548d461b8f91
Dravernuss <esteban16.rodas@gmail.com>
2025-02-08 17:17:20 -0500

    Creating form using CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import styles from "./App.module.css";
+import Form from "./components/Form/Form";
 
 function App() {
   return (
     <>
       <h1 className={styles.title}>Buscador de clima</h1>
       <div className={styles.container}>
-        <p>1</p>
-        <p>2</p>
+        <Form />
       </div>
     </>
   );
